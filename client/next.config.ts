8ad08c6dbd74dd8e024c026f6a57df58515bbd71
CEEPREL <ceeprel3@gmail.com>
2025-06-24 15:37:15 +0100

    Updates

--- client/next.config.ts
+++ client/next.config.ts
@@ -14,7 +14,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
+        source: '/:path*',
         destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
